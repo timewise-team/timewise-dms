70967191ec0bfaa4f23005241c90ea17930c63fa
Builanviet <builanviet@gmail.com>
2024-09-13 22:39:39 +0700

    push code workspace_user_handler

--- handlers/v1.go
+++ handlers/v1.go
@@ -2,7 +2,6 @@ package feature
 
 import (
 	_ "dbms/docs"
-	"dbms/handlers/schedule"
 	"dbms/handlers/user"
 	"dbms/handlers/workspace_user"
 	"github.com/gofiber/fiber/v2"
@@ -17,7 +16,6 @@ func RegisterHandlerV1(db *gorm.DB) *fiber.App {
 	v1 := router.Group("/dbms/v1")
 	v1.Get("/swagger/*", swagger.HandlerDefault)
 	user.RegisterUserHandler(v1.Group("/user"), db)
-	schedule.RegisterScheduleHandler(v1.Group("/schedule"), db)
 	workspace_user.RegisterWorkspaceUserHandler(v1.Group("/workspace_user"), db)
 	return router
 }
--- handlers/workspace_user/workspace_user_handler.go
+++ handlers/workspace_user/workspace_user_handler.go
@@ -54,39 +54,3 @@ func (h *WorkspaceUserHandler) removeWorkspaceUserById(c *fiber.Ctx) error {
 	}
 	return c.SendStatus(fiber.StatusNoContent)
 }
-
-func (h *WorkspaceUserHandler) createWorkspaceUser(c *fiber.Ctx) error {
-	workspaceUser := new(models.TwWorkspaceUser)
-	if err := c.BodyParser(workspaceUser); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	if result := h.DB.Create(workspaceUser); result.Error != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(result.Error.Error())
-	}
-
-	return c.JSON(workspaceUser)
-}
-
-func (h *WorkspaceUserHandler) updateWorkspaceUser(c *fiber.Ctx) error {
-	workspaceUser := new(models.TwWorkspaceUser)
-	if err := c.BodyParser(workspaceUser); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	if result := h.DB.Save(workspaceUser); result.Error != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(result.Error.Error())
-	}
-
-	return c.JSON(workspaceUser)
-}
-func (h *WorkspaceUserHandler) getWorkspaceUsersByWorkspaceId(c *fiber.Ctx) error {
-	var workspaceUsers []models.TwWorkspaceUser
-	workspaceId := c.Params("workspace_id")
-
-	if result := h.DB.Where("workspace_id = ?", workspaceId).Find(&workspaceUsers); result.Error != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(result.Error.Error())
-	}
-
-	return c.JSON(workspaceUsers)
-}
