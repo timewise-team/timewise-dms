53b73928294d648492f2057d966b7646c3fbbd59
anhnguyenduc5 <anh.nguyenduc.work@gmail.com>
2024-11-19 23:32:02 +0700

    Update notification_handler.go

--- handlers/notification/notification_handler.go
+++ handlers/notification/notification_handler.go
@@ -3,7 +3,6 @@ package notification
 import (
 	"errors"
 	"github.com/gofiber/fiber/v2"
-	"github.com/timewise-team/timewise-models/dtos/core_dtos"
 	"github.com/timewise-team/timewise-models/models"
 	"gorm.io/gorm"
 )
@@ -19,23 +18,15 @@ type NotificationHandler struct {
 // @Accept json
 // @Produce json
 // @Param createNotificationRequest body core_dtos.PushNotificationDto true "Create notification request"
-// @Success 200 {object} core_dtos.PushNotificationDto
+// @Success 200 {object} models.TwNotifications
 // @Router /dbms/v1/notification [post]
 func (h *NotificationHandler) CreateNotification(c *fiber.Ctx) error {
 	// Get data from request
-	var request core_dtos.PushNotificationDto
+	var request models.TwNotifications
 	if err := c.BodyParser(&request); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
-	notification := models.TwNotifications{
-		UserEmailId:     request.UserEmailId,
-		Type:            request.Type,
-		Message:         request.Message,
-		RelatedItemId:   request.RelatedItemId,
-		RelatedItemType: request.RelatedItemType,
-		ExtraData:       request.ExtraData,
-	}
-	if result := h.DB.Create(&notification); result.Error != nil {
+	if result := h.DB.Create(&request); result.Error != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": result.Error.Error()})
 	}
 	// Insert data into database
